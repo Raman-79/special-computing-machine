4e583a1a7a585a64b280b6237869c9bee1e26a25
Raman Singh <118994443+Raman-79@users.noreply.github.com>
2025-06-03 17:45:03 +0530

    Update something.py

--- something.py
+++ something.py
@@ -1,8 +1,1 @@
-import {
-  BedrockAgentClient,
-  CreateAgentCommand,
-  GetAgentCommand,
-  paginateListAgents,
-} from "@aws-sdk/client-bedrock-agent";
 
-const client = new BedrockAgentClient({ region: "us-east-1" });
